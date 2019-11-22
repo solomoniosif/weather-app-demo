2ad006f8ee710bf04b2ae5731626a8740803d06f
Iosif Solomon <solomoniosif@gmail.com>
2019-11-22 23:04:50 +0200

    Added option to check weather for any city

--- app.py
+++ app.py
@@ -1,21 +1,70 @@
+import weather as w
+from flask import Flask, render_template, request
 import json
 
-from flask import Flask
-
-import weather
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8e532a95c1956ca19d07c3a4cc6f0fe9'
 
 @app.route("/")
-def hello():
-    return "Salut! \nDe unde ne comandam de mancare azi?"
-
-@app.route("/weather/")
-def weathe_route():
-    temp = json.dumps(weather.weather())
-    return temp
+def home():
+    return render_template('home.html')
 
 
-@app.route("/weather/my-cities")
-def weather_multiple_cities():
-    return "Cluj: 15, New York: 10"
+@app.route("/cluj/")
+def cluj():
+    weather_cluj = w.weather_cluj()
+    return render_template('presentweather.html', weather=weather_cluj)
+
+
+@app.route("/lisbon/")
+def lisbon():
+    weather_lisbon = w.weather_lisbon()
+    return render_template('presentweather.html', weather=weather_lisbon)
+
+@app.route("/tirana/")
+def tirana():
+    weather_tirana = w.weather_tirana()
+    return render_template('presentweather.html', weather=weather_tirana)
+
+@app.route("/dublin/")
+def dublin():
+    weather_dublin = w.weather_dublin()
+    return render_template('presentweather.html', weather=weather_dublin)
+
+
+@app.route("/budapest/")
+def budapest():
+    weather_budapest = w.weather_budapest()
+    return render_template('presentweather.html', weather=weather_budapest)
+
+@app.route("/berlin/")
+def berlin():
+    weather_berlin = w.weather_berlin()
+    return render_template('presentweather.html', weather=weather_berlin)
+
+
+@app.route("/bergen/")
+def bergen():
+    weather_bergen = w.weather_bergen()
+    return render_template('presentweather.html', weather=weather_bergen)
+
+
+@app.route("/reqcity/", methods=["GET", "POST"])
+def reqcity():
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_city = w.weather_other(city)
+        return render_template('presentweather.html', weather=weather_city)
+    return render_template('reqcity.html')
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+# @app.route("/weather/my-cities")
+# def weather_multiple_cities():
+#     return "Cluj-Napoca: 15, New York: 10"
--- weather.py
+++ weather.py
@@ -1,3 +1,185 @@
-def weather():
-    return {"Cluj-Napoca": 15}
+import requests
+import json
+from secret import appid
 
+def weather_cluj():
+    city = 'Cluj-Napoca'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_lisbon():
+    city = 'Lisbon'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_tirana():
+    city = 'Tirana'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_dublin():
+    city = 'Dublin'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_budapest():
+    city = 'Budapest'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_berlin():
+    city = 'Berlin'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_bergen():
+    city = 'Bergen'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_other():
+    try:
+        city = input('Enter a city to check the weather')
+    except:
+        return 'No data could be found for your entry. Try again!'
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
+
+
+def weather_other(city):
+    
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    response = requests.get(url.format(city, appid))
+    r = response.json()
+
+    weather_d = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'pressure': r['main']['pressure'],
+        'humidity': r['main']['humidity'],
+        'temp_min': r['main']['temp_min'],
+        'temp_max': r['main']['temp_max'],
+        'wind_speed': r['wind']['speed']
+    }
+    return weather_d
